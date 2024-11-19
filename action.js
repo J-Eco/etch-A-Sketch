327788c25a6a9889d37163b6feb5a57a08002c3f
J-Eco <justezpa@gmail.com>
2024-11-18 22:21:10 -0800

    adding updated etch board

--- action.js
+++ action.js
@@ -4,3 +4,26 @@ for (let i = 0; i < 256; i++) {
     div.setAttribute('class', 'newDiv');
     container.appendChild(div);
 }
+
+if (document.querySelector('.newDiv')) {
+    const divGrid = document.querySelectorAll('.newDiv');
+    divGrid.forEach((element) => {
+        element.addEventListener('mouseover', () => {
+            boostOpacity(element);
+        })
+    })
+}
+
+function boostOpacity(ele) {
+    ele.style.backgroundColor = 'rgba(0, 255, 180)';
+    if (ele.style.opacity <= 0.9) {
+        ele.style.opacity = +ele.style.opacity + 0.1;
+    }
+}
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    const response = prompt("Num of Squares per side?")
+    container.remove();
+})
